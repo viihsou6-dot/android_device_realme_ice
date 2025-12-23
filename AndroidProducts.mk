d2f4203e18325100c4336172e85c45ceb57800dd
viihsou6-dot <viihsou6@gmail.com>
2025-12-23 22:41:50 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_ice.mk
+    $(LOCAL_DIR)/lineage_ice.mk
 
 COMMON_LUNCH_CHOICES := \
-    yaap_ice-user \
-    yaap_ice-userdebug \
-    yaap_ice-eng
+    lineage_ice-user \
+    lineage_ice-userdebug \
+    lineage_ice-eng
